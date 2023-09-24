8ac2a44eec2da1163a57e988eecfe0b0841960f5
Joel-SD <joelcarrasco.sd@gmail.com>
2023-09-24 18:40:41 -0500

    todos mock

---
+++ src/_mock/_todos.ts
@@ -0,0 +1,22 @@
+import { Todo } from "../types/todo";
+
+export const _todos: Todo[] = [
+  {
+    id: 1,
+    title: "todo 1",
+    description: "description 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "todo 2",
+    description: "description 2",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "todo 3",
+    description: "description 3",
+    completed: false,
+  },
+];
