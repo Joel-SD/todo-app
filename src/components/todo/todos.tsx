d6044014daf0b18e7781b80f550c6fb36ffccf94
Joel-SD <joelcarrasco.sd@gmail.com>
2023-10-01 14:40:44 -0500

    fetching free todo api, on selected logic

--- src/components/todo/todos.tsx
+++ src/components/todo/todos.tsx
@@ -1,27 +1,64 @@
-import ToDo from "./components/todo";
-import { Todo } from "@/types/todo";
-
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import ToDo from "./components/todo/todo";
 import styles from "./todosStyles.module.css";
+import { TodoType } from "@/types/todo.type";
 
-interface TodoProps {
-  onRemoveTodo: (id: number) => void;
-  todos: Todo[];
-}
+const ToDos = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
 
-const ToDos = ({ todos, onRemoveTodo }: TodoProps) => {
+  const getTodoData = async () => {
+    await fetch("https://dummyjson.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "fetch data");
+        setTodos(data.todos);
+        return data;
+      })
+      .catch((err) => {
+        console.log(err, "error fetch data");
+      });
+  };
+
+  useEffect(() => {
+    getTodoData();
+  }, []);
+
+  const handleRemove = (id: number) => {
+    console.log(id);
+    const newTodos = todos?.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const handleComplete = useCallback(
+    (id: number) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      setTodos(newTodos);
+    },
+    [todos]
+  );
+
   return (
     <div className={styles.container}>
+      <h1>Todo list </h1>
       <ul className={styles.todoListContainer}>
-        {todos.map((todo) => (
-          <ToDo
-            key={todo.id}
-            id={todo.id}
-            title={todo.title}
-            description={todo.description}
-            completed={todo.completed}
-            onRemoveTodo={onRemoveTodo}
-          />
-        ))}
+        {todos &&
+          todos.map((todo) => (
+            <ToDo
+              key={todo.id}
+              id={todo.id}
+              todo={todo.todo}
+              completed={todo.completed}
+              userId={todo.userId}
+              // onRemoveTodo={handleRemove}
+              onCompleteTodo={handleComplete}
+            />
+          ))}
       </ul>
     </div>
   );
