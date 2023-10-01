e95e9ff54e48259e1997a97470b8205a9f456b89
Joel-SD <joelcarrasco.sd@gmail.com>
2023-10-01 14:39:41 -0500

    removiendo logica de todo del main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useState } from "react";
 //styles
 import styles from "./page.module.css";
 //components
@@ -8,17 +6,9 @@ import ToDos from "@/components/todo/todos";
 import { _todos } from "@/_mock/_todos";
 
 export default function Home() {
-  const [todos, setTodos] = useState(_todos);
-
-  const handleRemove = (id: number) => {
-    console.log(id);
-    const newTodos = _todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos);
-  };
   return (
     <main className={styles.main}>
-      <h1>Lista de To-dos</h1>
-      <ToDos onRemoveTodo={handleRemove} todos={todos} />
+      <ToDos />
     </main>
   );
 }
